44602fe5eef997bf6c4b81f388095b3e9b2e52a2
Antonevich <v.antonevich@mail.ru>
2022-04-04 20:04:40 +0300

    решение задачи

---
+++ Array of strings/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу, которая из массива строк формирует массив из строк, длина которых меньше либо равна трём символам.
+
+using System;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите массив из строк ");
+            string text = Console.ReadLine();
+             string[] spled = text.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            
+            var res = from word in spled
+                      where word.Length <= 3
+                      select word;
+            if (res.Count() == 0)
+            {
+                Console.WriteLine("Значения меньше, либо равные трем символам отсутствуют");
+            }
+            else
+            {
+                foreach (string s in res)
+                {
+                    Console.WriteLine("Занчения которые отвечают критерию запроса  " + s);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
