d158d3794575aa44176a6f2b0c4d836a2760f6ca
Antonevich <v.antonevich@mail.ru>
2022-04-04 19:03:42 +0300

    Решение задачи

---
+++ Prog/Program.cs
@@ -0,0 +1,15 @@
+﻿// Находим максимальное значение из a,b,c,d.
+
+int a = 23;
+int b = 12;
+int c = 341;
+int d = 45;
+
+int max = a;
+
+if (b>a)   max = b;
+if (c>b)    max = c;
+if (d>c)  max = d;
+
+Console.WriteLine(max + "  данное число является максимальным из заданных чисел");
+
